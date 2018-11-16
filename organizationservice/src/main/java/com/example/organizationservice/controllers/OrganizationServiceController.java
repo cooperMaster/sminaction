ea3ac7391bb14b724a548e5297a907d42c53ee6c
whoyiming <375416934@qq.com>
2018-11-16 17:14:38 +0800

    add files

---
+++ organizationservice/src/main/java/com/example/organizationservice/controllers/OrganizationServiceController.java
@@ -0,0 +1,45 @@
+package com.example.organizationservice.controllers;
+
+
+import com.example.organizationservice.model.Organization;
+import com.example.organizationservice.services.OrganizationService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value="v1/organizations")
+public class OrganizationServiceController {
+    @Autowired
+    private OrganizationService orgService;
+
+
+    private static final Logger logger = LoggerFactory.getLogger(OrganizationServiceController.class);
+
+    @RequestMapping(value="/{organizationId}",method = RequestMethod.GET)
+    public Organization getOrganization(@PathVariable("organizationId") String organizationId) {
+        logger.debug(String.format("Looking up data for org %s", organizationId));
+
+        Organization org = orgService.getOrg(organizationId);
+        org.setContactName("OLD::" + org.getContactName());
+        return org;
+    }
+
+    @RequestMapping(value="/{organizationId}",method = RequestMethod.PUT)
+    public void updateOrganization(@PathVariable("organizationId") String orgId, @RequestBody Organization org) {
+        orgService.updateOrg( org );
+    }
+
+    @RequestMapping(value="/{organizationId}",method = RequestMethod.POST)
+    public void saveOrganization(@RequestBody Organization org) {
+       orgService.saveOrg( org );
+    }
+
+    @RequestMapping(value="/{organizationId}",method = RequestMethod.DELETE)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteOrganization(@PathVariable("orgId") String orgId, @RequestBody Organization org) {
+        orgService.deleteOrg( org );
+    }
+}
