590c832f923421ad638115f9eb1b72eefafa0317
whoyiming <375416934@qq.com>
2018-11-18 14:10:55 +0800

    update files

--- organizationservice/src/main/docker/run.sh
+++ organizationservice/src/main/docker/run.sh
@@ -2,20 +2,20 @@
 echo "********************************************************"
 echo "Waiting for the eureka server to start on port $EUREKASERVER_PORT"
 echo "********************************************************"
-while ! `nc -z eurekaserver $EUREKASERVER_PORT`; do sleep 3; done
+while ! `nc -z eurekaserver $EUREKASERVER_PORT`; do sleep 3; done;
 echo "******* Eureka Server has started"
 
 
 echo "********************************************************"
 echo "Waiting for the database server to start on port $DATABASESERVER_PORT"
 echo "********************************************************"
-while ! `nc -z database $DATABASESERVER_PORT`; do sleep 3; done
+while ! `nc -z database $DATABASESERVER_PORT`; do sleep 3; done;
 echo "******** Database Server has started "
 
 echo "********************************************************"
 echo "Waiting for the configuration server to start on port $CONFIGSERVER_PORT"
 echo "********************************************************"
-while ! `nc -z configserver $CONFIGSERVER_PORT`; do sleep 3; done
+while ! `nc -z configserver $CONFIGSERVER_PORT`; do sleep 3; done;
 echo "*******  Configuration Server has started"
 
 
@@ -35,11 +35,13 @@ echo "*******  Configuration Server has started"
 echo "********************************************************"
 echo "Starting Organization Service                           "
 echo "********************************************************"
-java -Djava.security.egd=file:/dev/./urandom -Dserver.port=$SERVER_PORT   \
-     -Deureka.client.serviceUrl.defaultZone=$EUREKASERVER_URI             \
-     -Dspring.cloud.config.uri=$CONFIGSERVER_URI                          \
-     -Dspring.profiles.active=$PROFILE                                   \
-#     -Dspring.cloud.stream.kafka.binder.zkNodes=$KAFKASERVER_URI          \
-#     -Dspring.cloud.stream.kafka.binder.brokers=$ZKSERVER_URI             \
-#     -Dspring.zipkin.baseUrl=$ZIPKIN_URI                                  \
-     -jar /usr/local/organizationservice/@project.build.finalName@.jar
+#java -Djava.security.egd=file:/dev/./urandom -Dserver.port=$SERVER_PORT   \
+#     -Deureka.client.serviceUrl.defaultZone=$EUREKASERVER_URI             \
+#     -Dspring.cloud.config.uri=$CONFIGSERVER_URI                          \
+#     -Dspring.profiles.active=$PROFILE                                   \
+##     -Dspring.cloud.stream.kafka.binder.zkNodes=$KAFKASERVER_URI          \
+##     -Dspring.cloud.stream.kafka.binder.brokers=$ZKSERVER_URI             \
+##     -Dspring.zipkin.baseUrl=$ZIPKIN_URI                                  \
+#     -jar /usr/local/organizationservice/@project.build.finalName@.jar
+# 使用上面的java -jar找不到main class 是由于windows下的换行和linux的不一样导致
+java -Djava.security.egd=file:/dev/./urandom -Dserver.port=$SERVER_PORT -Deureka.client.serviceUrl.defaultZone=$EUREKASERVER_URI -Dspring.cloud.config.uri=$CONFIGSERVER_URI -Dspring.profiles.active=$PROFILE -jar /usr/local/organizationservice/@project.build.finalName@.jar
